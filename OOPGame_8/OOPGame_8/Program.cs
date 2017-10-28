52eeacbe1f27bd87a3f1503a46a84a62a6f8add7
Andrew Eskenazi <drewesk@gmail.com>
2017-10-28 13:52:25 -0600

    Video 8 OOP BattleClassScenario

---
+++ OOPGame_8/OOPGame_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOPGame_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior maximus = new Warrior("Maximus", 1000, 120, 40);
+
+            Warrior bob = new Warrior("Bob", 1000, 120, 40);
+
+            Battle.StartFight(maximus, bob);
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
+/*
+Byte-Sized Notes
+
+Bob Attacks Maximus and Deals 74 Damage Maximus has 69 Health
+Maximus Attacks Bob and Deals 6 Damage Bob has 6 Health
+Bob Attacks Maximus and Deals 48 Damage Maximus has 21 Health
+Maximus Attacks Bob and Deals 48 Damage Bob has -42 Health
+Bob has Died and Maximus is Victorious
+Game Over
+*/
---
+++ OOPGame_8/OOPGame_8/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOPGame_8
+{
+    public class Warrior
+    {
+        // Name, Health, Attack Max Block Max
+        public string Name { get; set; } = "Warrior";
+        public double Health { get; set; } = 0;
+        public double AttkMax { get; set; } = 0;
+        public double BlockMax { get; set; } = 0;
+
+        // Randomization with numbers
+        Random rnd = new Random();
+
+        public Warrior(string name = "Warrior",
+                       double health = 0,
+                       double attkMax = 0,
+                       double blockMax = 0)
+        {
+            Name = name;
+            Health = health;
+            AttkMax = attkMax;
+            BlockMax = blockMax;
+        }
+
+        // Attacking
+        // Generate a random attack from 1 ot the Max
+        public double Attack()
+        {
+            return rnd.Next(1, (int)AttkMax);
+        }
+
+        // Blocking
+        // same as Attacking
+        public double Block()
+        {
+            return rnd.Next(1, (int)BlockMax);
+        }
+
+    }
+}
