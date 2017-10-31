9e6b2287f38cbc1a6415fc070fbb140c652af642
Andrew Eskenazi <drewesk@gmail.com>
2017-10-30 20:04:20 -0600

    Video 9

---
+++ Polymorphism/Polymorphism/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Polymorphism
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+
+        public abstract double Area();
+    }
+}
